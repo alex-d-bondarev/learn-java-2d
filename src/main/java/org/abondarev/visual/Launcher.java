c16832064a18543817dff0f3102bd3bf605ec576
abondarev-sel <abondarev@determine.com>
2017-10-29 23:30:52 +0200

    lesson 1

---
+++ src/main/java/org/abondarev/visual/Launcher.java
@@ -0,0 +1,14 @@
+package org.abondarev.visual;
+
+import org.abondarev.visual.display.Display;
+
+/**
+ * See https://www.youtube.com/watch?v=dEKs-3GhVKQ&list=PLah6faXAgguMnTBs3JnEJY0shAc18XYQZ
+ *
+ */
+public class Launcher{
+    public static void main( String[] args )
+    {
+        new Display("Demo", 400, 400);
+    }
+}
