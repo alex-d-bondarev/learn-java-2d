da2d383f0145e154a495e8403caaa8370c555fb2
Alex <alex.d.bondarev@gmail.com>
2019-05-29 23:12:33 +0300

    complete chapter 18

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # learn-java-2d
 
 [Credit to CodeNMore 2D game programming tutorial](https://www.youtube.com/playlist?list=PLah6faXAgguMnTBs3JnEJY0shAc18XYQZ)
+
+How to run?
+-----------
+
+Just use [Launcher](./src/main/java/org/abondarev/visual/Launcher.java) main method
--- src/main/java/org/abondarev/visual/worlds/World.java
+++ src/main/java/org/abondarev/visual/worlds/World.java
@@ -1,11 +1,20 @@
 package org.abondarev.visual.worlds;
 
 import org.abondarev.visual.tiles.Tile;
+import org.abondarev.visual.utils.Utils;
 
 import java.awt.*;
 
 public class World {
-    private int width, height;
+    private static final int NUMBER_OF_RESERVED_FILE_VALUES = 4;
+    private static final int RESERVED_WIDTH_VALUE = 0;
+    private static final int RESERVED_HEIGHT_VALUE = 1;
+    private static final int RESERVED_SPAWN_X_VALUE = 2;
+    private static final int RESERVED_SPAWN_Y_VALUE = 3;
+
+    private static final String ALL_DELIMETERS = "\\s+";
+
+    private int width, height, spawnX, spawnY;
     private int[][] tiles;
 
     public World(String path){
@@ -32,14 +41,27 @@ public class World {
     }
 
     private void loadWorld(String path){
-        width = 5;
-        height = 5;
+        String file = Utils.loadFileAsAString(path);
+        String[] tokens = file.split(ALL_DELIMETERS);
+
+        // NUMBER_OF_RESERVED_FILE_VALUES:
+        width = Utils.parseInt(tokens[RESERVED_WIDTH_VALUE]);
+        height = Utils.parseInt(tokens[RESERVED_HEIGHT_VALUE]);
+
+        spawnX = Utils.parseInt(tokens[RESERVED_SPAWN_X_VALUE]);
+        spawnY = Utils.parseInt(tokens[RESERVED_SPAWN_Y_VALUE]);
+
+
         tiles = new int[width][height];
 
-        for(int x = 0; x < width; x++){
-            for(int y = 0; y < height; y++){
-                tiles[x][y] = 0;
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                tiles[x][y] = getTileTokenValue(x, y, tokens);
             }
         }
+    }
+
+    private int getTileTokenValue(int x, int y, String[] tokens){
+        return Utils.parseInt(tokens[(x + y * width) + NUMBER_OF_RESERVED_FILE_VALUES]);
     }
 }
